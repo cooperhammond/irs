58eef229e1a5528a823ec5eddb3d2c8273e5a687
Kepoor Hampond <kepoorh@gmail.com>
2017-06-10 18:08:02 -0700

    I hate getting stuff to work locally and remotely

--- setup.py
+++ setup.py
@@ -27,8 +27,8 @@ class PostInstallCommand(install):
         from shutil import copyfile
 
         print("\n\nDownloading Dependencies:\n")
-        ydl_binaries.download_ffmpeg("~/.irs/bin")
-        ydl_binaries.update_ydl("~/.irs/bin")
+        # ydl_binaries.download_ffmpeg("~/.irs/bin")
+        # ydl_binaries.update_ydl("~/.irs/bin")
 
         config_file = path.expanduser("~/.irs/config_.py")
         if not path.isfile(config_file):
