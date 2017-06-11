1d152f7109f47e79c8a0fcb95a0c0ff5ffe72550
Kepoor Hampond <kepoorh@gmail.com>
2017-06-11 14:03:56 -0700

    Make sure to call `irs --setup` before anything else

---
+++ irs/setup_binaries.py
@@ -0,0 +1,12 @@
+import ydl_binaries
+from shutil import copyfile
+import os
+
+
+def setup():
+    ydl_binaries.download_ffmpeg("~/.irs/bin")
+    ydl_binaries.update_ydl("~/.irs/bin")
+
+    config_file = os.path.expanduser("~/.irs/config_.py")
+    if not os.path.isfile(config_file):
+        copyfile("irs/config_preset", config_file)
