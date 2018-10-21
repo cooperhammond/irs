e0d01a1cfd408af066c414f184f20239bdd0e977
kepoorhampond <kepoorh@gmail.com>
2018-10-21 00:36:36 +0000

    fixed not recognizing ffmpeg

--- irs/setup_binaries.py
+++ irs/setup_binaries.py
@@ -6,8 +6,10 @@ import irs
 
 
 def setup():
-    ydl_binaries.download_ffmpeg("~/.irs/bin/")
-    ydl_binaries.update_ydl("~/.irs/bin/")
+    bin_path = os.path.expanduser("~/.irs/bin/")
+
+    ydl_binaries.download_ffmpeg(bin_path)
+    ydl_binaries.update_ydl(bin_path)
 
     config_file = os.path.expanduser("~/.irs/config_.py")
     if not os.path.isfile(config_file):
